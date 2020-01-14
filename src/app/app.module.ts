82db06134539aef9d9e4f3ab30f54c593e6df158
unknown <alessdianez@gmail.com>
2020-01-14 12:15:02 +0100

    ImagePicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(environment.firebase),
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(environment.firebase), AngularFireStorageModule,
     AngularFirestoreModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
+    providers: [
+      StatusBar,
+      SplashScreen,
+      ImagePicker,
+      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+   
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/firestore.service.ts
+++ src/app/firestore.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
 
-  constructor(private angularFirestore: AngularFirestore) { }
+  constructor(private angularFirestore: AngularFirestore, private angularFireStorage: AngularFireStorage) { }
   
   public insertar(coleccion, datos) {
     return this.angularFirestore.collection(coleccion).add(datos);
@@ -29,4 +31,12 @@ export class FirestoreService {
    public consultarId(coleccion,documentId){
      return this.angularFirestore.collection(coleccion).doc(documentId).snapshotChanges();
    }
+  public uploadImage(nombreCarpeta, nombreArchivo, imagenBase64){
+    let storageRef = this.angularFireStorage.ref(nombreCarpeta).child(nombreArchivo);
+    return storageRef.putString("data:image/jpeg;base64,"+imagenBase64, 'data_url');
+  }
+  public deleteFileFromURL(fileURL) {
+    return this.angularFireStorage.storage.refFromURL(fileURL).delete();
+  }
+  
 }
